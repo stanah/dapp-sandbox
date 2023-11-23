fd605d2bba82b114ce8f7fe35c56fa750c5589a5
stanah <foo.exe@gmail.com>
2023-11-23 23:04:58 +0900

    Refactor Web3Modal component signature

--- packages/frontend/src/context/Web3Modal.tsx
+++ packages/frontend/src/context/Web3Modal.tsx
@@ -24,6 +24,6 @@ const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
 // 3. Create modal
 createWeb3Modal({ wagmiConfig, projectId, chains });
 
-export function Web3Modal({ children }: any) {
+export function Web3Modal({ children }: { children: React.ReactNode }) {
   return <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>;
 }
