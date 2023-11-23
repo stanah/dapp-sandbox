31425e082e9071c2baa9c690c51f61a764287366
stanah <foo.exe@gmail.com>
2023-11-23 21:36:34 +0900

    use walletClient

--- packages/frontend/src/context/ethersAdapter.ts
+++ packages/frontend/src/context/ethersAdapter.ts
@@ -1,21 +1,16 @@
 import * as React from "react";
-import { type WalletClient, useWalletClient, useAccount } from "wagmi";
+import { type WalletClient, useWalletClient } from "wagmi";
 import { providers } from "ethers";
 
 export function walletClientToSigner(walletClient: WalletClient) {
-  // if (contractAddress == null || !isAddress(contractAddress)) throw new Error("CONTRACT_ADDRESS is not set");
-  const { address } = useAccount();
-  // const { account, chain, transport } = walletClient;
-  // const network = {
-  //   chainId: chain.id,
-  //   name: chain.name,
-  //   ensAddress: chain.contracts?.ensRegistry?.address,
-  // };
-  // const provider = new providers.Web3Provider(transport, network);
-  // const signer = provider.getSigner(account.address);
-  if (!window.ethereum) return undefined;
-  const provider = new providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner(address);
+  const { account, chain, transport } = walletClient;
+  const network = {
+    chainId: chain.id,
+    name: chain.name,
+    ensAddress: chain.contracts?.ensRegistry?.address,
+  };
+  const provider = new providers.Web3Provider(transport, network);
+  const signer = provider.getSigner(account.address);
   return signer;
 }
 
